71bcf50ac950412de30e820dd069e4496facd385
Eric <s10242231@gmail.com>
2019-12-02 18:09:19 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,37 @@ import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
 import './bus.js'
 import currencyFilter from './filters/currency'
+import VeeValidate from 'vee-validate'
+import zhTW from 'vee-validate/dist/locale/zh_TW'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false;
 axios.defaults.withCredentials = true;
 Vue.use(VueAxios, axios);
+Vue.use(VueI18n)
+
+
+
+const i18n = new VueI18n({
+  locale: 'zhTW'
+})
+Vue.use(VeeValidate, {
+  events: 'input|blur',
+  i18n,
+  dictionary: {
+    zhTW
+  } //這是為了讓使用者離開該欄位時觸發驗證
+});
+// VeeValidate.Validator.localize('zh_TW' ,zhTW)
+
 Vue.component('Loading' , Loading);
 Vue.filter('currency' , currencyFilter);
 
+
+
 new Vue({
   router,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
 
--- src/views/customPage.vue
+++ src/views/customPage.vue
@@ -34,41 +34,9 @@
                 </div>
             </div>
         </div>
-        <div class="col-8 mx-auto">
-            <table class="table mt-4 test">
-                <thead>       
-                    <th width="40"></th>        
-                    <th width="150">品名</th>
-                    <th width="90">數量</th>
-                    <th width="90">單價</th>
-                </thead>
-                <tbody>
-                    <tr v-for="(item, key) in cart.carts" :key="key">
-                        <td class="align-middle">
-                            <button type="button" class="btn btn-outline-danger btn-sm" @click="removeCart(item.id)">
-                                <i class="far fa-trash-alt"></i>
-                            </button>
-                        </td>
-                        <td class="align-middle"> {{item.product.title}} </td>
-                        <td>
-                            {{item.qty}} / {{item.product.unit}}                     
-                        </td>
-                        <td class="text-right">
-                            {{item.final_total | currency}}
-                        </td>
-                    </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="3" class="text-right">總計</td>
-                        <td class="text-right">{{ cart.total }}</td>
-                    </tr>
-                    <tr>
-                        <td colspan="3" class="text-right text-success">折扣價</td>
-                        <td class="text-right text-success">{{ cart.final_total }}</td>
-                    </tr>
-                </tfoot>
-            </table>
+        <div class="pull-right">
+        <button class="btn btn-primary btn-sm cartBtn" @click="cartModal">
+            <i class="fas fa-cart-plus"></i> <br> 購<br>物<br>車</button>
         </div>
         <!-- modal -->
         <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
@@ -110,7 +78,107 @@
                 </div>
             </div>
             </div>
-        </div>    
+        </div>
+        <!-- end -->
+        <!-- Cart -->
+        <div class="modal" id="cartModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">購物車</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="col-8 mx-auto">
+                        <table class="table mt-4 test">
+                            <thead>       
+                                <th width="40"></th>        
+                                <th width="150">品名</th>
+                                <th width="90">數量</th>
+                                <th width="90">單價</th>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(item, key) in cart.carts" :key="key">
+                                    <td class="align-middle">
+                                        <button type="button" class="btn btn-outline-danger btn-sm" @click="removeCart(item.id)">
+                                            <i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </td>
+                                    <td class="align-middle"> {{item.product.title}} 
+                                        <div class="text-success" v-if="item.coupon">
+                                            已套用優惠券
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {{item.qty}} / {{item.product.unit}}                     
+                                    </td>
+                                    <td class="text-right">
+                                        {{item.final_total | currency}}
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colspan="3" class="text-right">總計</td>
+                                    <td class="text-right">{{ cart.total }}</td>
+                                </tr>
+                                <tr v-if="cart.final_total !== cart.total">
+                                    <td colspan="3" class="text-right text-success">折扣價</td>
+                                    <td class="text-right text-success">{{ cart.final_total }}</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                            <div class="input-group mb-3 primary">
+                                <input type="text" class="form-control" placeholder="請輸入優惠碼" v-model="codes">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="setCoupon">套用優惠</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="my-5 row justify-content-center">
+                    <form class="col-md-6" @submit.prevent="createOrder">
+                        <div class="form-group">
+                        <label for="useremail">Email</label>
+                        <input type="email" v-validate="'required|email'" class="form-control" name="email" id="useremail"
+                            v-model="form.user.email" placeholder="請輸入 Email" required>
+                        <span class="text-danger" v-if="errors.has('email')">{{errors.first('email')}} </span>
+                        </div>
+                    
+                        <div class="form-group">
+                        <label for="username">收件人姓名</label>
+                        <input type="text" v-validate="'required'" class="form-control" name="name" id="username"
+                            v-model="form.user.name" placeholder="輸入姓名" :class="{'is-invalid' : errors.has('name')}">
+                        <span class="text-danger" v-if="errors.has('name')"> 請輸入姓名 </span>
+                        </div>
+                    
+                        <div class="form-group">
+                        <label for="usertel">收件人電話</label>
+                        <input type="tel" v-validate="'required'" class="form-control" name="phone" id="usertel" v-model="form.user.tel" placeholder="請輸入電話">
+                        <span class="text-danger" v-if="errors.has('phone')"> 請輸入電話號碼 </span>
+                        </div>
+                    
+                        <div class="form-group">
+                        <label for="useraddress">收件人地址</label>
+                        <input type="text" v-validate="'required'" class="form-control" name="address" id="useraddress" v-model="form.user.address"
+                            placeholder="請輸入地址">
+                        <span class="text-danger" v-if="errors.has('address')"> 請輸入地址 </span>
+                        </div>
+                    
+                        <div class="form-group">
+                        <label for="comment">留言</label>
+                        <textarea name="" id="comment" class="form-control" cols="30" rows="10" v-model="form.message"></textarea>
+                        </div>
+                        <div class="text-right">
+                        <button class="btn btn-danger">送出訂單</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- end -->
     </div>
 </template>
@@ -130,6 +198,15 @@ export default {
       cart:{
           carts: {},
       },
+      form:{
+          user:{
+              name:'',
+              email:'',
+              tel:'',
+              address:'',
+          }
+      },
+      codes:'',
     }
   },
   methods: {
@@ -160,8 +237,10 @@ export default {
             product_id : id ,
             qty
         }
-        this.$http.post(api, {data: cart }).then((response) => {
+        this.$http.post(api, { data: cart }).then((response) => {
             console.log(response.data);
+            console.log(cart);
+            
             this.status.loadingItem = '';  
             this.getCart();
             $('#productModal').modal('hide');
@@ -186,6 +265,30 @@ export default {
             this.getCart();
             this.isLoading = false;
         })
+    },
+    cartModal(){
+        $('#cartModal').modal('show');
+    },
+    setCoupon(code){
+        const api = `${process.env.VUE_APP_API}/api/erictest/coupon`
+        this.isLoading = true ;
+        const codes = {
+            code: this.codes
+        };    
+        this.$http.post(api, {data:codes} ).then((response) => {
+            console.log(response.data);
+            console.log(codes);
+            this.getCart();
+        })
+    },
+    createOrder(){
+        const api = `${process.env.VUE_APP_API}/api/erictest/order`;
+        const order = this.form;
+        this.isLoading = true;
+        this.$http.post(api , {data:order}).then((response) => {
+            console.log('訂單已建立' ,response);
+            this.isLoading = false;
+        })
     }
   },
   
@@ -197,5 +300,9 @@ export default {
 </script>
 
 <style scoped>
-
+.cartBtn{
+    position: fixed;
+    bottom: 250px;
+    right: 0px;
+}
 </style>
